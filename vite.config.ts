0a9e2898c734932f28a79efef5e0c6269bc42750
Brad Clark <bdashrad@gmail.com>
2025-02-05 14:22:24 -0500

    add mock deployer scripts

--- vite.config.ts
+++ vite.config.ts
@@ -8,15 +8,11 @@ import nightwatchPlugin from 'vite-plugin-nightwatch'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-    nightwatchPlugin(),
-  ],
+  base: '/swiftly-scented/', // Replace <REPO_NAME> with your GitHub repository name
+  plugins: [vue(), vueJsx(), vueDevTools(), nightwatchPlugin()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
